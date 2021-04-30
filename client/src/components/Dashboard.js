ff47c3d10e5cfa6bf5c206be45ced7534695f152
Weston Carpenter <wc.cc96@gmail.com>
2021-04-29 20:09:23 -0400

    updated login page (wip)

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-
 const Dashboard = () => {
-
   return (
-    <div >
-        Thanks for logging in!
-        <a href="/api/logout">Logout</a>
+    <div style={{textAlign: "center"}}>
+      <h2>Thanks for logging in!</h2>
+      <h4>This page is currently under construction.</h4>
+      <p>For more information please contact wc.cc96@gmail.com</p>
+      <a href="/api/logout">Logout</a>
     </div>
   );
 };
