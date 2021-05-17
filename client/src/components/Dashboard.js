0bc9a4e7dacbeb3e6f93775b85a9873e0df54234
Weston Carpenter <wc.cc96@gmail.com>
2021-05-16 21:17:49 -0400

    basic template for task creation flow

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,14 +1,154 @@
-import React from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepButton from '@material-ui/core/StepButton';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Data from "./Data";
 
-const Dashboard = () => {
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  title:{
+    flexGrow: 1,
+  },
+  button: {
+    marginRight: theme.spacing(1),
+  },
+  logoutButton: {
+    marginRight: theme.spacing(1),
+  },
+  completed: {
+    display: 'inline-block',
+  },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+}));
+
+function getSteps() {
+  return ['Upload Data', 'Customize Task', 'Publish Task'];
+}
+
+function getStepContent(step) {
+  console.log(step)
+}
+
+export default function HorizontalNonLinearStepper() {
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [completed, setCompleted] = React.useState({});
+  const steps = getSteps();
+
+  const totalSteps = () => {
+    return steps.length;
+  };
+
+  const completedSteps = () => {
+    return Object.keys(completed).length;
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps();
+  };
+
+  const handleNext = () => {
+    const newActiveStep =
+      isLastStep() && !allStepsCompleted()
+        ? // It's the last step, but not all steps have been completed,
+          // find the first step that has been completed
+          steps.findIndex((step, i) => !(i in completed))
+        : activeStep + 1;
+    setActiveStep(newActiveStep);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStep = (step) => () => {
+    setActiveStep(step);
+  };
+
+  const handleComplete = () => {
+    const newCompleted = completed;
+    newCompleted[activeStep] = true;
+    setCompleted(newCompleted);
+    handleNext();
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+    setCompleted({});
+  };
+
   return (
-    <div style={{textAlign: "center"}}>
-      <h2>Thanks for logging in!</h2>
-      <h4>This page is currently under construction.</h4>
-      <p>For more information please contact wc.cc96@gmail.com</p>
-      <a href="/api/logout">Logout</a>
+    <div className={classes.root}>
+       <AppBar position="static" color="transparent">
+        <Toolbar>
+          <Typography variant="h3" className={classes.title}>
+            U+AI
+          </Typography>
+          <Button className={classes.logoutButton} variant="outlined" color="inherit" href="/api/logout">Logout</Button>
+        </Toolbar>
+      </AppBar>
+      <Stepper nonLinear activeStep={activeStep}>
+        {steps.map((label, index) => (
+          <Step key={label}>
+            <StepButton onClick={handleStep(index)} completed={completed[index]}>
+              {label}
+            </StepButton>
+          </Step>
+        ))}
+      </Stepper>
+      <Data/>
+      <div>
+        {allStepsCompleted() ? (
+          <div>
+            <Typography className={classes.instructions}>
+              All steps completed - you&apos;re finished
+            </Typography>
+            <Button onClick={handleReset}>Reset</Button>
+          </div>
+        ) : (
+          <div>
+            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+            <div >
+              <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
+                Back
+              </Button>
+              <Button
+                variant="contained"
+                color="default"
+                onClick={handleNext}
+                className={classes.button}
+              >
+                Next
+              </Button>
+              {activeStep !== steps.length &&
+                (completed[activeStep] ? (
+                  <Typography variant="caption" className={classes.completed}>
+                    Step {activeStep + 1} already completed
+                  </Typography>
+                ) : (
+                  <Button variant="contained" color="default" onClick={handleComplete}>
+                    {completedSteps() === totalSteps() - 1 ? 'Finish' : 'Complete Step'}
+                  </Button>
+                ))}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
-};
-
-export default Dashboard;
+}
---
+++ client/src/components/Data.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { DataGrid } from '@material-ui/data-grid';
+
+const columns = [
+  { field: 'unit_id', headerName: 'ID', width: 70 },
+  { field: 'image_url', headerName: 'image_url', width: 130 },
+  { field: 'title', headerName: 'Last name', width: 130 },
+  { field: 'description', headerName: 'Last name', width: 130 }
+];
+
+const rows = [
+  { id: 1, image_url: 'https://miro.medium.com/max/640/0*B1nMAW5C3-S-W0a8.jpg', title: 'Test image', description: 'This is a test image' },
+];
+
+export default function () {
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+    </div>
+  );
+}
