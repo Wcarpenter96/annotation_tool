8e8da8eb20a87dc1f3d14541a5727c178f8350cb
Weston Carpenter <wc.cc96@gmail.com>
2021-07-13 21:50:18 -0400

    Add traits about an identified user in Logrocket

--- client/src/index.js
+++ client/src/index.js
@@ -16,6 +16,12 @@ LogRocket.init('6cej8a/my-first-project');
 
 setupLogRocketReact(LogRocket);
 
+LogRocket.identify('85608474-ce77-4a88-863f-dab2241403c7', {
+  name: 'Weston Carpenter',
+  email: 'wc.cc96@gmail.com',
+  developer:true
+});
+
 const store = createStore(reducers, {}, compose(applyMiddleware(reduxThunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),applyMiddleware(LogRocket.reduxMiddleware())));
 
 ReactDOM.render(
